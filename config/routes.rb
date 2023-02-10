7f465329f273f4155ea5334de10378b8c271f735
andisiambuku <roselandambuku@gmail.com>
2023-02-10 12:29:43 +0300

    feat: added product routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  #product routes
+  get'/products', to:'products#index'
+  get'/products/:id', to:'products#show'
+  put'update', to:'products#update'
+  post'/new', to:'products#create'
+  delete'/remove', to:'products#destroy'
+  
 
   # Defines the root path route ("/")
   # root "articles#index"
